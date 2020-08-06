dc537d4ec969a63e080a4cc72a0694f8d8ebebd8
AdamAugustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2020-08-06 17:21:59 -0300

    cria as paginas de dar aulas e estudar

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from '../Landing/styles';
+
+import PageHeader from '../../components/PageHeader';
+
+import './styles';
+
+function TeacherList() {
+  return (
+    <PageHeader title="Proffys disponiveis" />
+  );
+}
+
+export default TeacherList;
